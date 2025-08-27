cc3d2d2b89dd245f65589fd5ee90d7e941256e17
william.hasrouty <williamhasrouty@yahoo.com>
2025-08-26 18:09:26 -0700

    fix font size

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -1,10 +1,14 @@
 import "./DeleteModal.css";
+import closeBtn from "../../assets/close.svg";
 
 function DeleteModal({ onDeleteItem, card, onClose, activeModal }) {
   return (
     <div className={`modal ${activeModal === "delete" && "modal_opened"}`}>
       <div className="delete-modal">
         <div className="delete-modal__content">
+             <button onClick={onClose} type="button" className="modal__close">
+                      <img src={closeBtn} alt="Close button" />
+                    </button>
           <p className="delete__message">
             Are you sure you want to delete this item?
             <br />
